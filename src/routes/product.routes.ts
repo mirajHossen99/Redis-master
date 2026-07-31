c4c385850c93134aaa58b710a6949113a0ee801d
mirajHossen99 <mirajhossen211013@gmail.com>
2026-08-01 02:08:06 +0600

    redis-rate-limit

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import * as productController from "../controllers/product.controller";
+import { productRateLimiter } from "../middleware/rateLimit.middleware";
 
 const router = Router();
+
+router.use(productRateLimiter);
 
 router.get("/", productController.getProducts);
 router.get("/:id", productController.getProductById);
